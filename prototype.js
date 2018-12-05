68ac3553d33967aa7e4eb7f012233610056becf4
John Nolette <jnolette@transparent.com>
2018-12-05 12:18:29 -0500

    Adding lib prototype

---
+++ prototype.js
@@ -0,0 +1,56 @@
+// source: https://jsfiddle.net/neetjn/2zy8v0oa/
+
+function hyperTable(selectorDOM, columns, data) {
+	const el = typeof selectorDOM === 'object' ? selectorDOM : document.querySelector(selectorDOM)
+  const render = hyperHTML(el)
+  let index = {}
+  columns = columns.map((column, index) => Object.assign(column, {index: index}))
+  data = data.map(vector => {
+  	let units = []
+    for (var key in vector) {
+    	units.push([vector[key], columns.find(column => column.accessor === key).index])
+    }
+    return units
+  })
+	data.forEach((unit, index) => {
+  	data[index] = unit.sort((a, b) => a[1] > b[1])
+  })
+  render`
+    <table>
+      <thead>
+        <tr>${columns.map(column => hyperHTML.wire()`<th>${column.Header}</th>`)}</tr>
+      </thead>
+      <tbody>
+        ${data.map(unit => {
+        	return hyperHTML.wire()`<tr>${unit.map(item => {
+          	return hyperHTML.wire()`<td>${item[0]}</td>`
+          })}</tr>`
+        })}
+      </tbody>
+    </table>
+  `
+}
+
+const columns = [
+	{
+  	Header: 'Name',
+    accessor: 'name'
+  },
+  {
+  	Header: 'Age',
+    accessor: 'age'
+  }
+]
+
+const data = [
+	{
+  	name: 'John Doe',
+    age: 23,
+  },
+  {
+    age: 26,
+  	name: 'Jane Doe',
+  }
+]
+
+hyperTable('#table', columns, data)

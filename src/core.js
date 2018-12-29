cc8a6d5ebbc9ba36cfc0003b27713ea767f83ae0
John Nolette <jnolette@transparent.com>
2018-12-29 13:05:12 -0500

    Cleaned up table logic

--- src/core.js
+++ src/core.js
@@ -1,41 +1,42 @@
 import hyperHTML from 'hyperhtml'
 
-export function hyperTable(selectorDOM, columns, data) {
-  const el =
-    typeof selectorDOM === 'object'
-      ? selectorDOM
-      : document.querySelector(selectorDOM)
-  const html = hyperHTML(el)
-  columns = columns.map((column, index) =>
-    Object.assign(column, { index: index })
-  )
-  data = data.map(vector => {
-    const units = []
-    for (var key in vector) {
-      units.push([
-        vector[key],
-        columns.find(column => column.accessor === key).index
-      ])
-    }
-    return units
-  })
-  data.forEach((unit, index) => {
-    data[index] = unit.sort((a, b) => a[1] > b[1])
-  })
-  html`
-    <table>
-      <thead>
-        <tr>${columns.map(
-          column => hyperHTML.wire()`<th>${column.Header}</th>`
-        )}</tr>
-      </thead>
-      <tbody>
-        ${data.map(unit => {
-          return hyperHTML.wire()`<tr>${unit.map(item => {
-            return hyperHTML.wire()`<td>${item[0]}</td>`
-          })}</tr>`
-        })}
-      </tbody>
-    </table>
-  `
+export class Table {
+  constructor(props) {
+    this.props = props
+    this.html = hyperHTML.wire(this)
+  }
+  render() {
+    const columns = this.props.columns.map((column, index) =>
+      Object.assign(column, { index: index })
+    )
+    const data = this.props.data.map(vector => {
+      const units = []
+      for (var key in vector) {
+        units.push([
+          vector[key],
+          columns.find(column => column.accessor === key).index
+        ])
+      }
+      return units
+    })
+    data.forEach((unit, index) => {
+      data[index] = unit.sort((a, b) => a[1] > b[1])
+    })
+    return this.html`
+      <table>
+        <thead>
+          <tr>${columns.map(
+            column => hyperHTML.wire()`<th>${column.Header}</th>`
+          )}</tr>
+        </thead>
+        <tbody>
+          ${data.map(unit => {
+            return hyperHTML.wire()`<tr>${unit.map(item => {
+              return hyperHTML.wire()`<td>${item[0]}</td>`
+            })}</tr>`
+          })}
+        </tbody>
+      </table>
+    `
+  }
 }
